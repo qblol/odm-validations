ecc2067e037494176a365404515f48967dddcd52
qblol <iqbal.0809@gmail.com>
2017-02-24 06:40:17 +0700

    done done

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -15,7 +15,7 @@ module.exports = {
     list: function (req, res) {
         UserModel.find(function (err, Users) {
             if (err) {
-                return res.status(500).json({
+                return res.json({
                     message: 'Error when getting User.',
                     error: err
                 });
@@ -31,7 +31,7 @@ module.exports = {
         var id = req.params.id;
         UserModel.findOne({_id: id}, function (err, User) {
             if (err) {
-                return res.status(500).json({
+                return res.json({
                     message: 'Error when getting User.',
                     error: err
                 });
@@ -49,18 +49,26 @@ module.exports = {
      * UserController.create()
      */
     create: function (req, res) {
-        var User = new UserModel({			email : req.body.email,			username : req.body.username,			password : bcrypt.hashSync(req.body.password, saltRounds)
-        });
-
-        User.save(function (err, User) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating User',
-                    error: err
+        if(req.body.password.length > 8 && req.body.password.length < 16) {
+          var User = new UserModel({
+    			email : req.body.email,
+    			username : req.body.username,
+    			password : bcrypt.hashSync(req.body.password, saltRounds)
                 });
-            }
-            return res.status(201).json(User);
-        });
+          User.save(function (err, User) {
+              if (err) {
+                  return res.json({
+                      message: 'Invalid username & email',
+                      error: err
+                  });
+              }
+              return res.status(201).json(User);
+          });
+        } else {
+          return res.json({
+              message: 'Password must be within 8 to 16 characters'
+          });
+        }
     },
 
     /**
@@ -70,7 +78,7 @@ module.exports = {
         var id = req.params.id;
         UserModel.findOne({_id: id}, function (err, User) {
             if (err) {
-                return res.status(500).json({
+                return res.json({
                     message: 'Error when getting User',
                     error: err
                 });
@@ -84,7 +92,7 @@ module.exports = {
             User.email = req.body.email ? req.body.email : User.email;			User.username = req.body.username ? req.body.username : User.username;			User.password = req.body.password ? req.body.password : User.password;
             User.save(function (err, User) {
                 if (err) {
-                    return res.status(500).json({
+                    return res.json({
                         message: 'Error when updating User.',
                         error: err
                     });
@@ -102,7 +110,7 @@ module.exports = {
         var id = req.params.id;
         UserModel.findByIdAndRemove(id, function (err, User) {
             if (err) {
-                return res.status(500).json({
+                return res.json({
                     message: 'Error when deleting the User.',
                     error: err
                 });
